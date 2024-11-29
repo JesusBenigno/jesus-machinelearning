8628e6077536baad53cf572992b89aae709e8136
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-11-28 19:00:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ st.info('Esta aplicacion entrena un modelo de ML de renta de bicicletas y permit
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(df)
