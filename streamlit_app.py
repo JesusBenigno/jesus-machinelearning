e1d03cdffcaa06b66605becc1f23961d823692f2
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-11-28 18:38:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ st.title('WebApp para pronosticar demanda de bicicletas')
 st.info('Esta aplicacion entrena un modelo de ML de renta de bicicletas y permite hacer pronosticos!')
 
 df = pd.read_csv('https://github.com/JesusBenigno/jesus-machinelearning/blob/master/SeoulBikeData.csv')
+df
