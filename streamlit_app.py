efd92d3236e4d61f57f399475768e48a6120ca19
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-11-28 19:08:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,5 @@ st.title('WebApp para pronosticar demanda de bicicletas')
 
 st.info('Esta aplicacion entrena un modelo de ML de renta de bicicletas y permite hacer pronosticos!')
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
+df = pd.read_csv('https://raw.githubusercontent.com/JesusBenigno/jesus-machinelearning/refs/heads/master/data/day.csv')
+df
