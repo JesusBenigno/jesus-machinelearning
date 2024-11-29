41439f394f6b3917d366a54b874de670e237fa0b
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-11-28 22:24:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,29 @@ with st.expander('Data'):
   
 # Definir X_raw (season, weekday, weathersit, temp, hum, windspeed)
 x_raw = df.drop('cnt', axis=1)
-
 # Definir Y_raw (cnt)
 y_raw = df.cnt
 
+
 with st.expander('**Visualizacion de Data**'):
   st.scatter_chart(data=df, x='season', y='cnt', height = 500)
+
+# Input features por el usuario para pronosticar
+with st.sidebar:
+  st.header('Features de entrada')
+  season = st.selectbox('Season', ('Spring','Summer','Fall','Winter'))
+  weekday = st.selectbox('Weekday', ('Monday', 'Tuesday', 'Wednesady', 'Thursday', 'Friday', 'Saturday', 'Sunday'))
+  weathersit = st.selectbox('Weather', ('Clear', 'Cloudy', 'Rainy', 'Heavy Rain'))
+  temp = st.slider('Temp. (Normalizada)', 0.06, 0.86, 0.5)
+  hum = st.slider('Hum. (%)', 0, 1, 0.6)
+  windspeed = st.slider('Vel. Viento (Normalizada)', 0.02, 0.5, 0.2)
+
+
+
+
+
+
+
+
 
 
