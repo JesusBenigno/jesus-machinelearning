afb6ca00c136f420e649c1079a3c84ec3a905e0d
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-11-30 21:00:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,9 +97,7 @@ st.success(int(updated_res))
 #Mostrar grafica
 xLR = x_raw['Temperature(°C)'].values
 xLR
-yLR = regressor.predict(xLR)
-yLR
-#fig = plt.figure(xLR, regressor.predict(xLR), color = 'blue')
+fig = plt.figure(xLR, regressor.predict(y_pred), color = 'blue')
 #fig.title('Verdad o mentira (Regresion Lineal Multiple Temo. vs Bicis Rentadas)')
 #fig.xlabel('Temp.(°C)')
 #fig.ylabel('Bicis Rentadas')
