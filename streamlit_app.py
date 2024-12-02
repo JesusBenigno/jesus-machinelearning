ee95093cd3a4f699a6e44f2c0c425aad6249cd3b
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-12-01 21:18:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,7 @@ plt.ylabel('Bicis Rentadas')
 st.pyplot(fig)
 
 st.subheader('Regresion Lineal Polinomial')
-grado = st.selectbox('Grado del Polinomio', (1,2,3))
+grado = st.selectbox('Grado del Polinomio: (optimo=2)', (1,2,3))
 poly_reg = PolynomialFeatures(degree=grado)
 X_poly = poly_reg.fit_transform(X_train)
 lin_reg_2 = LinearRegression()
