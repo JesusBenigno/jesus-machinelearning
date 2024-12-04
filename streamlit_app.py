6760536da3e92887ed9ec16ee27d20ea044e6e58
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2024-12-04 16:46:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,7 +137,7 @@ for j in temps:
   arr_predicts1.append(res_for)
   
 fig1,ax1 = plt.subplots()
-ax1.scatter(x_raw['Solar Radiation (Mj/m2)'], y_raw, color = 'red')
+ax1.scatter(x_raw['Solar Radiation (MJ/m2)'], y_raw, color = 'red')
 ax1.plot(temps, arr_predicts1, color = 'blue')
 plt.title('Verdad o mentira (Regresion Lineal Polinomial Temp. vs Bicis Rentadas)')
 plt.xlabel('Solar Rad')
